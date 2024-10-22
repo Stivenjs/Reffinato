8ed56f0d031e9cdcac778c97426a2c3d323dc864
jajaiders <jajaiders@gmail.com>
2024-10-22 15:13:19 -0500

    Creacion del custom hook para el distado de los productos en el panel de administrador

--- src/pages/admin/AdminProductList.jsx
+++ src/pages/admin/AdminProductList.jsx
@@ -1,33 +1,15 @@
-import { useEffect, useState } from "react";
 import axiosInstance from "../../instances/axiosInstance";
-import { toast } from "@/hooks/use-toast";
 import { Button } from "@/components/ui/button";
+import { toast } from "@/hooks/use-toast";
 import SideBar from "../../components/shared/SideBar";
+import useProductList from "../../hooks/useProductList";
+
+
 
 const AdminProductList = () => {
-  const [productList, setProductList] = useState([]);
 
-  const fetchProducts = async () => {
-    try {
-      const { data } = await axiosInstance.get("/products/list");
-
-      if (data) {
-        setProductList(data);
-      } else {
-        toast({
-          title: "Error",
-          description: data.message,
-          variant: "destructive",
-        });
-      }
-    } catch (error) {
-      toast({
-        title: "Error",
-        description: error.message,
-        variant: "destructive",
-      });
-    }
-  };
+  const {productList, fetchProducts} = useProductList();
+  console.log()
 
   const deleteProduct = async (id) => {
     try {
@@ -48,19 +30,14 @@ const AdminProductList = () => {
       });
     }
   };
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
   return (
-    <div className="flex flex-row">
-      <SideBar/>
-      <div className="flex flex-col gap-2 w-[80%] mt-24 mb-9 relative left-3">
-      <b className="mb-2">Products list</b>
-        {/* titulo de la tabla */}
+    <div className="flex flex-col md:flex-row">
+      <SideBar />
+      <div className="flex flex-col gap-2 w-full md:w-[80%] mt-24 mb-9 relative md:left-3 px-4">
+        <b className="mb-2 text-lg">Products list</b>
+
         <div className="hidden md:grid grid-cols-[1fr_0.9fr_0.89fr_0.88fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm">
-          <b>image</b>
+          <b>Image</b>
           <b>Name</b>
           <b>Color</b>
           <b>Price</b>
@@ -68,26 +45,29 @@ const AdminProductList = () => {
           <b>Category</b>
           <b className="text-center">Action</b>
         </div>
-        {/* Prouct list */}
-        {productList.map((item, x) => (
+
+        {productList.map((item, index) => (
           <div
-            key={x}
-            className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr_1fr] md:grid-cols-[1fr_1fr_1fr_1fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm"
+            key={index}
+            className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-[1fr_1fr_1fr_1fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm"
           >
-            <img src={item.photos[0]} alt="Product image" className="w-12 h-20" />
+            <img
+              src={item.photos[0]}
+              alt="Product image"
+              className="w-16 h-16 md:w-12 md:h-20 object-cover"
+            />
             <p>{item.name}</p>
-            <p>{item.color}</p>
+            <p className="hidden sm:block">{item.color}</p>
             <p>{item.price}</p>
-            <p>{item.sizes}</p>
-            <p>{item.category}</p>
-            <div className="flex gap-2">
+            <p className="hidden sm:block">{item.sizes.join(", ")}</p>
+            <p className="hidden sm:block">{item.category}</p>
+            <div className="flex gap-2 justify-center">
               <Button
                 onClick={() => deleteProduct(item.id)}
-                className="bg-red-600 w-24"
+                className="bg-red-600 w-24 text-xs sm:text-sm"
               >
                 Delete
               </Button>
-              <Button className="w-24">Edit</Button>
             </div>
           </div>
         ))}
